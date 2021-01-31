34f1bf93e9835147260f7f3c5d1f8717b54b7593
riefer02 <andrew.riefenstahl@gmail.com>
2021-01-31 09:56:00 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="container flex-col mx-auto bg-red-200">
+    <div class="w-full bg-indigo-200">
+      <h1 class="text-5xl">Nebulae</h1>
+      <h3 class="text-3xl mt-2">What are nebulae?</h3>
+    </div>
+    <div class="flex w-full bg-yellow-200 p-4">
+      <div
+        alt="Helix Nebula"
+        class="w-1/3 bg-cover bg-center"
+        style="
+          background-image: url(https://cdn.mos.cms.futurecdn.net/iuQmVPLBRbmRQeoA46VTZ6.jpg);
+        "
+      ></div>
+      <div class="bg-green-200 w-2/3 p-2">
+        <p class="text-2xl">
+          Our solar system began in a nebula about 15 billion years ago. All the
+          matter in the universe was packed into an incredibly small, dense
+          area. A huge explosion happened, and everything scattered in all
+          directions. Over billions of years, gravity pulled space dust and
+          gases together into clouds. These huge, shining clouds are called
+          nebulae—  a single cloud is called a nebula. Many stars, like our sun,
+          were born in the clouds of the nebulae. 
+        </p>
+      </div>
+    </div>
+    <div class="bg-indigo-200 p-4">
+      <p class="text-2xl">
+        The largest, most violent star-forming region known lies in the
+        Tarantula Nebula. The bright knot of stars to the left of the center
+        contains many of the largest, hottest, and brightest stars known. Click
+        through the slideshow to see a variety of known nebulae found in our
+        galaxy and beyond. 
+      </p>
+    </div>
+    <div class="w-full p-4 flex">
+      <Slider />
+      <div class="flex flex-col justify-evenly items-center p-4">
+        <p class="text-xl">
+          <span class="text-2xl block">Directions:</span> Click through the
+          images of the Nebula on the image slider to learn more.
+        </p>
+        <p class="text-xl">
+          <span class="text-2xl block">Question:</span> What are some
+          differences you notice between the nebulae in the pictures?
+        </p>
+        <button
+          class="text-3xl rounded-lg shadow-lg hover:shadow-sm bg-indigo-600 hover:bg-indigo-500 mx-auto p-2 text-gray-200 mt-2 focus:outline-none"
+          @click="toggleAnswer()"
+        >
+          Click Here
+        </button>
+        <transition name="fade">
+          <p v-show="showAnswer" class="text-xl mt-2">
+            <span class="text-2xl block relative top-0 left-0">Answer:</span>
+            They have different colors, shapes, and amounts of brightness
+          </p>
+        </transition>
+      </div>
+    </div>
+    <div class="bg-yellow-200 w-full flex justify-center">
+      <div class="flex">
+        <button class="text-xl mr-4">Previous Page</button>
+        <button class="text-xl">Next Page</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Slider from "./components/Slider.vue";
+export default {
+  components: {
+    Slider,
+  },
+  data() {
+    return {
+      showAnswer: true,
+    };
+  },
+  methods: {
+    toggleAnswer() {
+      this.showAnswer = !this.showAnswer;
+    },
+  },
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ src/components/Slider.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="slider w-1/2">
+    <transition-group tag="div" name="fade" class="slides-group">
+      <div
+        :key="current"
+        class="slide text-3xl"
+        :class="slides[current].className"
+      >
+        <p class="text-grey-200 text-opacity-50">{{ slides[current].name }}</p>
+      </div>
+    </transition-group>
+    <div
+      class="btn btn-prev text-5xl"
+      aria-label="Previous slide"
+      @click="slide(-1)"
+    >
+      &#10094;
+    </div>
+    <div
+      class="btn btn-next text-5xl"
+      aria-label="Next slide"
+      @click="slide(1)"
+    >
+      &#10095;
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    current: 0,
+    direction: 1,
+    slides: [
+      { className: "nebula-1", name: "Orion Nebula" },
+      { className: "nebula-2", name: "Crab Nebula" },
+      { className: "nebula-3", name: "Ring Nebula" },
+      { className: "nebula-4", name: "Carina Nebula" },
+      { className: "nebula-5", name: "Horsehead Nebula" },
+    ],
+  }),
+  methods: {
+    slide(dir) {
+      this.direction = dir;
+      dir === 1
+        ? (this.transitionName = "slide-next")
+        : (this.transitionName = "slide-prev");
+      var len = this.slides.length;
+      this.current = (this.current + (dir % len) + len) % len;
+    },
+  },
+  mounted() {
+    this.show = true;
+  },
+};
+</script>
+
+<style scoped>
+.nebula-1 {
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Orion_Nebula_-_Hubble_2006_mosaic_18000.jpg/600px-Orion_Nebula_-_Hubble_2006_mosaic_18000.jpg");
+}
+
+.nebula-2 {
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Crab_Nebula.jpg/600px-Crab_Nebula.jpg");
+}
+.nebula-3 {
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Hubble_image_of_the_Ring_Nebula_%28Messier_57%29.jpg/600px-Hubble_image_of_the_Ring_Nebula_%28Messier_57%29.jpg");
+}
+.nebula-4 {
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/The_spectacular_star-forming_Carina_Nebula_imaged_by_the_VLT_Survey_Telescope.jpg/559px-The_spectacular_star-forming_Carina_Nebula_imaged_by_the_VLT_Survey_Telescope.jpg");
+}
+.nebula-5 {
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Hubble_Sees_a_Horsehead_of_a_Different_Color.jpg/574px-Hubble_Sees_a_Horsehead_of_a_Different_Color.jpg");
+}
+
+/* FADE IN */
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-enter {
+  opacity: 0;
+}
+
+/* GO TO NEXT SLIDE */
+.slide-next-enter-active,
+.slide-next-leave-active {
+  transition: transform 0.5s ease-in-out;
+}
+.slide-next-enter {
+  transform: translate(100%);
+}
+.slide-next-leave-to {
+  transform: translate(-100%);
+}
+
+/* GO TO PREVIOUS SLIDE */
+.slide-prev-enter-active,
+.slide-prev-leave-active {
+  transition: transform 0.5s ease-in-out;
+}
+.slide-prev-enter {
+  transform: translate(-100%);
+}
+.slide-prev-leave-to {
+  transform: translate(100%);
+}
+
+.slider {
+  height: 400px;
+  position: relative;
+  overflow: hidden;
+  margin: 0;
+  color: #fff;
+}
+
+.slide {
+  width: 100%;
+  height: 400px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-position: 50% 50%;
+  background-size: cover;
+  object-fit: fill;
+}
+
+.btn {
+  z-index: 10;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  position: absolute;
+  top: calc(50% - 35px);
+  left: 1%;
+  transition: transform 0.3s ease-in-out;
+  user-select: none;
+}
+
+.btn-next {
+  left: auto;
+  right: 1%;
+}
+
+.btn:hover {
+  transform: scale(1.1);
+}
+</style>
